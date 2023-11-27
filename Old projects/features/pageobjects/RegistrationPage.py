8b2d191e775924a721b98ade75dadae50585d276
karthikkumaran123 <152186446+karthikkumaran123@users.noreply.github.com>
2023-11-27 20:23:28 +0530

    new 1.1
    updated new

--- Old projects/features/pageobjects/RegistrationPage.py
+++ Old projects/features/pageobjects/RegistrationPage.py
@@ -3,9 +3,6 @@ from features.pageobjects.BasePage import BasePage
 
 class RegistrationPage(BasePage):
 
-    def __init__(self,driver):
-        super().__init__(driver)
-
 
     def open(self,url):
         self.driver.get(url)
